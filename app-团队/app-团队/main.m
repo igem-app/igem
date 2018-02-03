4feb45600d488c6d528e057a629b2f3744dba384
guorf17 <36029367+guorf17@users.noreply.github.com>
2018-02-03 18:02:48 +0800

    app-团队页面

---
+++ app-团队/app-团队/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app-团队
+//
+//  Created by grf on 2018/2/3.
+//  Copyright © 2018年 grf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
